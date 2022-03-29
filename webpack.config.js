c848e45e962ef2a759246c8cea091cbd83d91a40
tysonrm <tyson.midboe@gmail.com>
2022-03-28 23:45:20 -0500

    fix MF config

--- webpack.config.js
+++ webpack.config.js
@@ -48,13 +48,14 @@ var serverConfig = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'customer',
+      name: 'distributed-cache',
       library: { type: 'commonjs-module' },
       filename: 'remoteEntry.js',
       exposes: {
-        './models': './src/domain',
-        './adapters': './src/adapters',
-        './services': './src/services'
+        './model-cache': './src/domain',
+        './adapter-cache': './src/adapters',
+        './service-cache': './src/services',
+        './event-bus': './src/services/event-bus'
       },
       shared: {
         axios: {
